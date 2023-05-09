c63bf8e0408062aa3177fe99c5df35eb89319048
jussitkuosa <jussi.kuosa@raseko.fi>
2023-05-09 09:42:27 +0300

    Lesson10 / JK versio 1.0

---
+++ lesson10.py
@@ -0,0 +1,11 @@
+#Laita koodin alkuun aina allaoleva Header 
+#Päivitä päivämäärä sekä omat nimikirjaimet ja koodin toimintakuvaus (lesson) 
+#*****************************************************  
+# Lesson10.py 
+# @author tekija: (ensimmaisen version tekija, JK)  
+# @since pvm: (versio pvm 9.5.2023)  
+# @version versio: (versionumero 1.0)  
+# muutos: (JK)  
+#*****************************************************/
+
+#tähän ratkaisukoodi
